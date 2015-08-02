db81dba1df312b2e5245c690663742f2f7d13704
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2015-08-01 22:02:45 -0500

    added title to pages, refactored tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
-    get :home
-    assert_response :success
+    static_page_test :home, "Home"
   end
 
   test "should get help" do
-    get :help
-    assert_response :success
+    static_page_test :help, "Help"
   end
 
+  test "should get about" do
+    static_page_test :about, "About"
+  end
+
+  private
+
+  def static_page_test(name, page_title)
+    get name
+    assert_response :success
+    assert_select "title", title(page_title)
+  end
+
+  def title(page)
+    "#{page} | Ruby on Rails Tutorial Sample App"
+  end
 end
